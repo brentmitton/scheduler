c5928b69d5eb7570c80b022236961af7bd73382e
Brent Mitton <brent.mitton@gmail.com>
2013-06-06 16:27:01 -0300

    Working a bit on the parsing.
    I'm pulling out seperate rows for labs and classes right now. Run into
    some issues with courses that are cross listed, so I need to fiddle
    with those a bit.

--- scheduler/courses/views.py
+++ scheduler/courses/views.py
@@ -1,20 +1,35 @@
-import requests
-import lxml
-from lxml import html
 from courses.models import Department, Course, Lab
 import xml.etree.ElementTree as ET
 from django.http import HttpResponse
+from BeautifulSoup import BeautifulSoup, NavigableString
+import urllib2
 
 FALL_URL = "https://secure.upei.ca/cls/dropbox/FallTime.html"
 SPRING_URL = "https://secure.upei.ca/cls/dropbox/SpringTime.html"
 
 def scrape(request, semester):
     if semester == 1:
-        r = requests.get(FALL_URL)
+        soup = BeautifulSoup(urllib2.urlopen(FALL_URL))
     else:
-        r = requests.get(SPRING_URL)
+        soup = BeautifulSoup(urllib2.urlopen(SPRING_URL))
 
-    root = lxml.html.fromstring(r.content)
+    tables = soup.findAll('table',attrs={"width":"100%"})
+    table = tables[0]
 
-    print root
+    # get all non-lab rows
+    course_rows = table.findAll(lambda tag: tag.name == "tr" and not tag.attrs)
+    lab_rows = table.findAll(lambda tag: tag.name == "tr" and tag.attrs)
+
+    for row in course_rows:
+        columns = row.findAll("td")
+        if len(columns)>0:
+            print "Code: %s -- Name: %s -- Location: %s -- Time: %s --  Status: %s -- Instructor: %s" %(parse_course_code(columns[0]), columns[1].string, columns[2].string, columns[3].string, columns[4].string, columns[5].string)
     return HttpResponse("Success")
+
+def parse_course_code(code):
+    if code.string == None:
+        code_list = str(code).split("<br />")
+        cleaned_string = " ".join(code_list)
+        print cleaned_string
+    else:
+        return code.string
